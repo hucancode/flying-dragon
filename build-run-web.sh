c34432324ea0f8de1ec9be5eefbce3badcbc176f
hucancode <hucancode@gmail.com>
2025-01-01 22:49:17 +0900

    helpful message for wasm build script

--- build-run-web.sh
+++ build-run-web.sh
@@ -1,4 +1,5 @@
 !#/usr/bin/sh
+which wasm-bindgen python3 || { echo "python3 and wasm-bindgen is required"; exit 1; }
 cargo build --target wasm32-unknown-unknown
 wasm-bindgen --out-dir web --target web ./target/wasm32-unknown-unknown/debug/flying-dragon.wasm
 python3 -m http.server --directory web
